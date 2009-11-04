1e5198d7994f8ccb1502671a9860f0268be50a49
Eric Davis <edavis@littlestreamsoftware.com>
2009-11-03 17:18:06 -0800

    [#3264] Setup test env and ported object_daddy exemplars from Redmine

--- init.rb
+++ init.rb
@@ -1,5 +1,22 @@
 require 'redmine'
 
+if Rails.env == "test"
+  
+  # Bootstrap ObjectDaddy since it's needs to load before the Models
+  # (it hooks into ActiveRecord::Base.inherited)
+  require 'object_daddy'
+
+  # Use the plugin's exemplar_path :nodoc:
+  module ::ObjectDaddy
+    module RailsClassMethods
+      def exemplar_path
+        File.join(File.dirname(__FILE__), 'test', 'exemplars')
+      end
+    end
+  end
+end
+
+
 Redmine::Plugin.register :redmine_merge_redmine do
   author 'Eric Davis'
   url 'https://projects.littlestreamsoftware.com/projects'
