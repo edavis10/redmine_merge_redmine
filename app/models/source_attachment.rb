6840cfc6db2030052214bf1752bfb4b872e8ee71
Riceball LEE <snowyu.lee@gmail.com>
2011-05-04 09:06:15 +0800

    * [bug] fixs the attach author and a little robustness improvements

--- app/models/source_attachment.rb
+++ app/models/source_attachment.rb
@@ -6,20 +6,30 @@ class SourceAttachment < ActiveRecord::Base
 
   def self.migrate
     all.each do |source_attachment|
+      next if Attachment.find(:first, :conditions => {:filename => source_attachment.filename,
+        :disk_filename => source_attachment.disk_filename,
+        :digest => source_attachment.digest,
+        :container_type => source_attachment.container_type})
 
       Attachment.create!(source_attachment.attributes) do |a|
-        a.author = User.find_by_login(source_attachment.author.login)
+        a.author = User.find_by_mail(source_attachment.author.mail)
+        a.author = User.find_by_login(source_attachment.author.login) if a.author.blank?
         a.container = case source_attachment.container_type
                       when "Issue"
-                        Issue.find RedmineMerge::Mapper.get_new_issue_id(source_attachment.container_id)
+                        id = RedmineMerge::Mapper.get_new_issue_id(source_attachment.container_id)
+                        Issue.find id if id
                       when "Document"
-                        Document.find RedmineMerge::Mapper.get_new_document_id(source_attachment.container_id)
+                        id = RedmineMerge::Mapper.get_new_document_id(source_attachment.container_id)
+                        Document.find id if id
                       when "WikiPage"
-                        WikiPage.find RedmineMerge::Mapper.get_new_wiki_page_id(source_attachment.container_id)
+                        id = RedmineMerge::Mapper.get_new_wiki_page_id(source_attachment.container_id)
+                        WikiPage.find id if id
                       when "Project"
-                        Project.find RedmineMerge::Mapper.get_new_project_id(source_attachment.container_id)
+                        id = RedmineMerge::Mapper.get_new_project_id(source_attachment.container_id)
+                        Project.find id if id
                       when "Version"
-                        Version.find RedmineMerge::Mapper.get_new_version_id(source_attachment.container_id)
+                        id = RedmineMerge::Mapper.get_new_version_id(source_attachment.container_id)
+                        Version.find id if id
                       end
 
       end
